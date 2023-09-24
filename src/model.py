1c257f9f3aa8bbe7e4586f32a1556f9c6bc8db29
Dima Birenbaum <dvlasenko86@gmail.com>
2023-09-24 23:33:50 +0300

    Using GPU for PyTorch

--- src/model.py
+++ src/model.py
@@ -13,6 +13,8 @@ class DistilBERTModel:
         self.model = transformers.AutoModelForQuestionAnswering.from_pretrained(model_name)
         self.trt_engine = None
         self.model_name = model_name
+        # Check if CUDA is available and set the device accordingly
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     def get_model_name(self):
         return self.model_name
@@ -38,8 +40,17 @@ class DistilBERTModel:
         return output
 
     def convert_to_onnx(self, onnx_path, inputs):
-        # Convert the PyTorch model to ONNX format
-        torch.onnx.export(self.model, (inputs["input_ids"], inputs["attention_mask"]), onnx_path, verbose=True, opset_version=11, input_names=['input_ids', 'attention_mask'], output_names=['output'])
+        # Move inputs to the same device as the model
+        inputs = {name: tensor.to(self.device) for name, tensor in inputs.items()}
+
+        # Export the model to ONNX
+        torch.onnx.export(self.model,
+                          (inputs["input_ids"], inputs["attention_mask"]),
+                          onnx_path,
+                          verbose=True,
+                          opset_version=11,
+                          input_names=['input_ids', 'attention_mask'],
+                          output_names=['output'])
 
     def build_engine(self, onnx_file_path, fp16_mode=False):
         # Create a TensorRT logger
